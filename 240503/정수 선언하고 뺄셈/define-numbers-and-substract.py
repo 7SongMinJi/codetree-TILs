e283e43312e1f7488d372be11237a5599a799ce4
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-03 20:03:06 +0900

    Title: 정수 선언하고 뺄셈, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/정수 선언하고 뺄셈/define-numbers-and-substract.py
@@ -0,0 +1,23 @@
+# a = 5
+# print(a)
+# print(type(a)) # <class 'int'>
+
+# b = "toy"
+# print(b)
+# print(type(b)) # <class 'str'>
+
+# c = 3.141592
+# print(c)
+# print(type(c)) # <class 'float'>
+
+# d = 5
+# e = 9
+# f = d + e
+# print("f =", f)
+
+# a, b = 5, 9
+# c = a + b
+# print("c =", c)
+
+a, b = 97, 13
+print(a, "-", b, "=", a-b)
