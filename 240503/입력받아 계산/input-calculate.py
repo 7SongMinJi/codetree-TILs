5cba3418f9a00d7c4a0df89251e78e3554412e78
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-03 20:51:37 +0900

    Title: 입력받아 계산, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/입력받아 계산/input-calculate.py
@@ -0,0 +1,21 @@
+# # input()을 통해서 "한 줄 단위"로 입력 받을 수 있음
+# # 문자열 type으로 받음!!
+# # 그래서 input 값 저장한 변수 + 1 불가능!! 에러 발생 ***
+# # => 입력받은 문자열을 숫자형 int로 바꿔주자!
+
+# a = input()
+# a = int(a) # 문자열->숫자형 int로 변경
+
+# print(f"A is {a}")
+
+# a = input()
+# a = int(a)
+# print(a + 1)
+
+# a = int(input())
+# print(a + 1)
+
+
+# 문제
+a = int(input())
+print(a + 2)
