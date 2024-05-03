16818342a30a8414393595236ac6d7ca148c9a0e
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-03 20:03:35 +0900

    Title: 세 정수형 변수 선언, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/세 정수형 변수 선언/declaration-of-three-natural-numbers.py
@@ -0,0 +1,35 @@
+# # 1
+# a = 5
+# print("A is %d" % a)
+
+# b = "apple"
+# print("B is %s" % b)
+
+# print("A is %d and B is %s" % (a, b))
+
+# # 2
+# a, b = 5, "apple"
+
+# print("A is {0}".format(a))
+# print("A is {new_a}".format(new_a=a))
+
+# print("B is {0}".format(a))
+# print("B is {new_b}".format(new_b=b))
+
+# print("A is {0} and B is {1}".format(a, b))
+# print("A is {new_a} and B is {new_b}".format(new_a=a, new_b=b))
+
+# print("B is {1} and A is {0}".format(a, b))
+# print("B is {new_b} and A is {new_a}".format(new_a=a, new_b=b))
+# print("B is {new_b} and A is {new_a}".format(new_b=b, new_a=a))
+
+# # 3
+# a, b = 5, "apple"
+
+# print(f"A is {a}")
+# print(f"B is {b}")
+# print(f"A is {a} and B is {b}")
+
+# 문제
+a, b, c = 7, 23, 30
+print(f"{a} + {b} = {c}")
