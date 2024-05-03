f3004689be6024afb1b0f170e051aab63251f21e
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-03 21:02:33 +0900

    Title: 실수 받아 그대로 출력, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/실수 받아 그대로 출력/enter-real-value-and-print.py
@@ -0,0 +1,17 @@
+# a = input()
+
+# a = float(a)
+
+# print(a + 0.58)
+
+
+# 문제
+
+n = float(input())
+
+# 아래 세 가지 표현 모두 가능!!! ***
+print(f"{n:.2f}")
+
+# print("{:.2f}".format(n))
+
+# print("%.2f" % n)
