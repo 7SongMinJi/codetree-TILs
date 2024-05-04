b8d6dd2ada94ea2c8f60e972e85da7a570a93ebc
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-04 17:51:09 +0900

    Title: 입력받아 계산 3, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240504/입력받아 계산 3/input-calculate-3.py
@@ -0,0 +1,15 @@
+# 2개의 줄에 걸쳐 입력
+
+# 원래는 파이썬 - 입력 한 줄 단위로 받음
+# 2개의 줄에 걸쳐 입력 받기 위해서는 그냥 input() 함수 2번 사용하기
+
+# a = input()
+# b = input()
+# print(f'A is {a}, B is {b}')
+
+
+# 문제
+a = int(input())
+b = int(input())
+
+print(a * b)
