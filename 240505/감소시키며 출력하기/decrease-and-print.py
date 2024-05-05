5878addcdb113e348504710b607effe8c001431a
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-05 18:14:43 +0900

    Title: 감소시키며 출력하기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240505/감소시키며 출력하기/decrease-and-print.py
@@ -0,0 +1,14 @@
+# # for문 1씩 감소
+
+# # range(a, b, -1): a부터 b + 1 ***까지 1씩 감소
+# # b는 출력되지 않음 ***
+
+# for i in range(10, 4, -1):
+#     print(i)
+# print("Done")
+# # 10 - 5 & Done 출력됨
+
+
+# 문제
+for i in range(17, 4, -1):
+    print(i, end=" ")
