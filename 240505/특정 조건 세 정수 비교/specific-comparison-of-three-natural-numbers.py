f0c9b6268122524f32d5b7e2fbd5ec2baad4777d
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-05 14:42:47 +0900

    Title: 특정 조건 세 정수 비교, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240505/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -2,11 +2,21 @@ arr = input().split()
 
 a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
 
-min = a
-if b < min:
-    min = b
-elif c < min:
-    min = c
+# min은 내장함수 이름이라 변수 이름으로 사용하지 않는 것을 권장!! ***
+# min_val = a
+# if b < min_val:
+#     min_val = b
+#     if c < min_val:
+#         min_val = c
+# elif c < min_val:
+#     min_val = c
 
-print(int(a == min), end=" ")
+min_val = a
+
+if b < min_val:
+    min_val = b
+if c < min_val:
+    min_val = c
+    
+print(int(a == min_val), end=" ")
 print(int(a == b and b == c))
