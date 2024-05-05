46ea49189c2a10606a3138ef6ef1e5ea476b2bf5
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-05 23:03:43 +0900

    Title: 369 게임, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/369 게임/369-game.py
+++ 240505/369 게임/369-game.py
@@ -1,7 +1,27 @@
+# n = int(input())
+
+# for i in range(1, n + 1):
+#     if i % 3 == 0 or (i // 10 == 3 or i // 10 == 6 or i // 10 == 9 or (i % 10) // 1 == 3 or (i % 10) // 1 == 6 or (i % 10) // 1 == 9):
+#         print(0, end=" ")
+#     else:
+#         print(i, end=" ")
+
+
+# 정답 코드
+# 변수 선언, 입력
 n = int(input())
 
-for i in range(1, n + 1):
-    if i % 3 == 0 or (i // 10 == 3 or i // 10 == 6 or i // 10 == 9 or (i % 10) // 1 == 3 or (i % 10) // 1 == 6 or (i % 10) // 1 == 9):
-        print(0, end=" ")
-    else:
-        print(i, end=" ")
+# 출력
+for i in range(1, n+1):
+	if i % 3 == 0:
+		print("0", end=" ")
+	elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+		print("0", end=" ")
+	elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+		print("0", end=" ")
+	else:
+		print(i, end=" ")
+
+
+# 아 그냥 i % 10 하면 일의 자리 수 나오는 거지!...
+# i가 3, 6, 9 중 하나를 포함하는 수는 i를 10으로 나누었을 때 나머지 또는 몫이 3, 6, 9일 때.
