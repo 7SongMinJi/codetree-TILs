9af0497c13b810f6c7c284e61a7b8b8ef8fe6470
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-05 18:08:07 +0900

    Title: 증가시키며 출력하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240505/증가시키며 출력하기/increase-and-print.py
@@ -0,0 +1,21 @@
+# # for loop 2씩 증가
+
+# # range 함수 세 번째 값 = 반복 변수 얼만큼 점프할지
+# # range(a, b, 2) = a부터 b-1까지 2씩 증가하며 뜀
+
+# for i in range(5, 11, 2):
+#     print(i)
+# print("Done")
+# # 5 7 9 Done 출력
+
+# for i in range(5, 12, 2):
+#     print(i)
+# print("Done")
+# # 5 7 9 11 Done 출력
+
+# # range(a, b)은 range(a, b, 1)과 같은 표현
+
+
+# 문제
+for i in range(5, 18, 2):
+    print(i, end=" ")
