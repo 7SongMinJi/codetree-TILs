804685cc439481e7b75351caae0e520b3e7a8da7
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-06 13:58:49 +0900

    Title: 1까지 나누기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240506/1까지 나누기/divide-by-1.py
+++ 240506/1까지 나누기/divide-by-1.py
@@ -1,7 +1,21 @@
-n = int(input())
+# n = int(input())
 
+# for i in range(1, 101):
+#     n = n // i
+#     if n <= 1:
+#         print(i)
+#         break
+
+
+# 정답 코드
+# 변수 선언 및 입력
+n = int(input())
+cnt = 0
+
+# 1부터 증가시키며 나눈 값이 1이하가 된 순간, 나눗셈을 진행한 횟수를 출력합니다.
 for i in range(1, 101):
-    n = n // i
     if n <= 1:
-        print(i)
+        print(cnt)
         break
+    n //= i
+    cnt += 1
