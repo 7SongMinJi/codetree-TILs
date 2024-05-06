d174dd579ed652a4a311254705bfc424b0afea75
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-06 15:37:31 +0900

    Title: 2의 거듭제곱수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240506/2의 거듭제곱수/pow-of-2.py
+++ 240506/2의 거듭제곱수/pow-of-2.py
@@ -9,3 +9,21 @@ while True:
     x += 1
 
 print(x)
+
+
+# 정답 코드
+
+# 변수 선언 및 입력
+n = int(input())
+prod = 1
+x = 0
+
+while True:
+	# prod가 n이 될 때까지 2를 곱합니다.
+	if n == prod:
+		break
+
+	prod *= 2
+	x += 1
+
+print(x)
