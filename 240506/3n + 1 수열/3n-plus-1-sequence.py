09c28fb9574f9a0ac7ed56e720ff62bb55e613e4
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-06 15:05:58 +0900

    Title: 3n + 1 수열, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240506/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,40 @@
+# 언제 끝날지 모르는 경우
+
+# 특정 코드를 몇 번 반복해야 하는지 모르겠을 때 무한 루프 이용 & 원하는 조건을 만족했을 때 break로 반복문 탈출하도록 함
+
+# 내가 적은 코드
+# n = int(input())
+# while True:
+#     n //= 2
+#     if n % 2 == 1:
+#         print(n)
+#         break
+
+# 자료 코드
+# n = int(input())
+# while True:
+#     print(f"current val is {n}")
+#     if n % 2 == 0:
+#         print("n is even")
+#         n = n // 2
+#     else:
+#         print("n is odd")
+#         break
+# print(n)
+
+
+# 문제
+n = int(input())
+
+cnt = 0
+
+while True:
+    if n % 2 == 0:
+        n //= 2
+    else:
+        n = n * 3 + 1
+    cnt += 1
+    if n == 1:
+        break
+
+print(cnt)
