273bc4484983f3e76cc70d65757cf05a834faefd
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-06 14:44:56 +0900

    Title: 몫 출력하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240506/몫 출력하기/print-share.py
+++ 240506/몫 출력하기/print-share.py
@@ -12,15 +12,36 @@
 # # 그 이유는 EOFError - 더 이상 받을 입력이 없음에도 input을 시도할 때 발생
 
 
-cnt = 0
+# cnt = 0
 
+# while True:
+#     try:
+#         n = int(input())
+#         if n % 2 == 0:
+#             if cnt == 3:
+#                 break
+#             print(n // 2)
+#             cnt += 1
+#     except EOFError:
+#         break
+
+
+
+# # 정답 코드
+# # 변수 선언 및 입력
+cnt = 0
+	
 while True:
-    try:
-        n = int(input())
-        if n % 2 == 0:
-            if cnt == 3:
-                break
-            print(n // 2)
-            cnt += 1
-    except EOFError:
-        break
+	# 변수 선언 및 입력
+	n = int(input())
+	
+	# n이 홀수라면 아무 작업도 하지 않고, n이 짝수라면 n/2를 출력하는 작업을 3번 한 뒤 종료합니다.
+    # 
+	if n % 2 == 1:
+		continue
+		
+	print(n // 2)
+	cnt += 1
+		
+	if cnt >= 3:
+		break
