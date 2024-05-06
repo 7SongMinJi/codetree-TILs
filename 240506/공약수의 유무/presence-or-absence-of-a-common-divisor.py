9bbc1dc61a91f41ec45c37c1d2246d6ef79d09a7
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-06 15:55:35 +0900

    Title: 공약수의 유무, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240506/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,16 @@
+# 범위 내 존재 여부 = Bool 함수 이용!!!
+
+arr = input().split()
+
+a, b = int(arr[0]), int(arr[1])
+
+bool_val = False
+
+for i in range(a, b + 1):
+    if 1920 % i == 0 or 2880 % i == 0:
+        bool_val = True
+
+if bool_val == True:
+    print(1)
+else:
+    print(0)
