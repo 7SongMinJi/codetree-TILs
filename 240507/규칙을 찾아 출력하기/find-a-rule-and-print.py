48b992ca88136da37aa26eb8d5936cc8434756a5
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-07 18:24:17 +0900

    Title: 규칙을 찾아 출력하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240507/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -7,3 +7,18 @@ for i in range(n):
         else:
             print("*", end=" ")
     print()
+
+
+# # 정답 코드
+
+# # 변수 선언 및 입력
+# n = int(input())
+	
+# # n칸의 정사각형에서 i, j가 조건에 맞으면 *을 출력합니다.
+# for i in range(n):
+# 	for j in range(n):
+# 		if i > j or i == 0 or j == n - 1:
+# 			print("* ", end="")
+# 		else:
+# 			print("  ", end="")
+# 	print()
