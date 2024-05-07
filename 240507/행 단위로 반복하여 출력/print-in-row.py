fc7b7a61219cdea9f5df35813ff01b56409b9525
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-07 18:27:32 +0900

    Title: 행 단위로 반복하여 출력, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240507/행 단위로 반복하여 출력/print-in-row.py
@@ -0,0 +1,15 @@
+# # 숫자 모양 출력
+
+# for i in range(2):
+#     for j in range(5):
+#         print(j + 1, end="")
+#     print()
+
+
+# 문제
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        print(j + 1, end="")
+    print()
