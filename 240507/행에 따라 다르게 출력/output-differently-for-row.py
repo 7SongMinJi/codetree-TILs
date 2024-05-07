1f3e20bb88bde301cdaf0b9eb9c1c4ab91b34bc2
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-07 22:07:13 +0900

    Title: 행에 따라 다르게 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240507/행에 따라 다르게 출력/output-differently-for-row.py
+++ 240507/행에 따라 다르게 출력/output-differently-for-row.py
@@ -13,3 +13,20 @@ for i in range(n):
             print(cnt, end=" ")
             cnt += 1
     print()
+
+
+# 정답 코드
+
+# 변수 선언 및 입력
+n = int(input())
+cnt = 0
+	
+# 값을 조건대로 증가시켜 출력합니다.
+for i in range(n):
+    for j in range(n):
+        if i % 2 == 0:
+            cnt += 1
+        else:
+            cnt += 2
+        print(cnt, end=" ")
+    print()
