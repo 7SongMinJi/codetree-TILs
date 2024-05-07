36b65d49eedc170656b97a5f5dfca86163e2f130
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-07 13:38:12 +0900

    Title: 정사각형 별표 출력하기, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/정사각형 별표 출력하기/print-stars-in-square.py
@@ -0,0 +1,9 @@
+# 이중 for문
+
+for _ in range(3):
+    for _ in range(5):
+        print(1, end="")
+    print()
+
+# 바깥 for문(i)은 행(세로줄 방향)에 관여 & 안쪽 for문(j)은 열(가로줄 방향)에 관여함
+# 주로 2중 for문을 사용하게 되는 경우 변수를 i, j 이렇게 2개를 사용함
