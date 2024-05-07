8f9a1403bfddb5de94f17fcc8cb54d64f8892b16
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-07 13:58:02 +0900

    Title: 별표 출력하기 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/별표 출력하기 2/print-star-2.py
+++ 240507/별표 출력하기 2/print-star-2.py
@@ -24,13 +24,13 @@ for i in range(n, 0, -1):
     print()
 
 
-# 정답 코드
+# # 정답 코드
 
-# 변수 선언 및 입력
-n = int(input())
+# # 변수 선언 및 입력
+# n = int(input())
 
-# 길이가 n인 직각삼각형을 출력합니다.
-for i in range(n):
-	for _ in range(n-i):
-		print("*", end=" ")
-	print()
+# # 길이가 n인 직각삼각형을 출력합니다.
+# for i in range(n):
+# 	for _ in range(n-i):
+# 		print("*", end=" ")
+# 	print()
