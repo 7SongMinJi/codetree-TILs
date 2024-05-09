18beb936d18fdf8fcf8b2820f0a8663966bd7250
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-09 15:15:28 +0900

    Title: 배열 채우기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/배열 채우기/filling-array.py
+++ 240509/배열 채우기/filling-array.py
@@ -11,12 +11,25 @@
 
 # // 하나씩 입력받는 경우에만 이렇게 짤 수 있는 거. 문제에선 한 줄로 쭉 주어지는 거니까 이렇게 짜면 안 됨!
 
+# arr = list(map(int, input().split()))
+
+# for elem in arr:
+#     if elem == 0:
+#         arr.pop()
+#         break
+
+# for elem in arr[::-1]:
+#     print(elem, end=" ")
+
+# // 중간에 0이 있고 그 뒤로도 있는 경우 이렇게 하면 틀림!!
+
 arr = list(map(int, input().split()))
 
-for elem in arr:
-    if elem == 0:
-        arr.pop()
+i = 0
+
+for i in range(len(arr)):
+    if arr[i] == 0:
         break
 
-for elem in arr[::-1]:
+for elem in arr[i-1::-1]:
     print(elem, end=" ")
