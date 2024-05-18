7674441a4413e81571b975d408735ec2d59bb629
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-18 21:06:02 +0900

    Title: 두 배열의 곱, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/두 배열의 곱/multiple-of-two-arrays.py
+++ 240518/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,14 +1,50 @@
-arr_2d_1 = [
-    list(map(int, input().split()))
-    for _ in range(3)
+# arr_2d_1 = [
+#     list(map(int, input().split()))
+#     for _ in range(3)
+# ]
+
+# input()
+
+# arr_2d_2 = [
+#     list(map(int, input().split()))
+#     for _ in range(3)
+# ]
+
+# for i in range(3):
+#     for j in range(3):
+#         print(arr_2d_1[i][j] * arr_2d_2[i][j], end=" ")
+#     print()
+
+
+# 정답 코드 - 중간에 공백 한 줄 그냥 입력 받으려면 input() 해주기!! ***
+
+# 첫 번째 2차원 배열을 구현해 정수를 입력받습니다.
+arr_1 = [
+	list(map(int, input().split()))
+	for _ in range(3)
 ]
+
 input()
-arr_2d_2 = [
-    list(map(int, input().split()))
+
+# 두 번째 2차원 배열을 구현해 정수를 입력받습니다.
+arr_2 = [
+	list(map(int, input().split()))
+	for _ in range(3)
+]
+
+# 2차원 배열을 구현합니다.
+arr_3 = [
+    [0 for _ in range(3)]
     for _ in range(3)
 ]
 
+# 두 배열의 곱을 새로운 배열에 담습니다.
 for i in range(3):
-    for j in range(3):
-        print(arr_2d_1[i][j] * arr_2d_2[i][j], end=" ")
-    print()
+	for j in range(3):
+		arr_3[i][j] = arr_1[i][j] * arr_2[i][j]
+	
+# 새로운 배열을 출력합니다.
+for row in arr_3:
+	for elem in row:
+		print(elem, end=" ")
+	print()
