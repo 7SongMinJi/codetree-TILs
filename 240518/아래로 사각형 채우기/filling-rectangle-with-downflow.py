ac62cf5bdab312e185f54e15addcebf42c6d33d9
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-18 21:33:21 +0900

    Title: 아래로 사각형 채우기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ 240518/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -46,26 +46,26 @@ for row in arr_2d:
     print()
 
 
-# 정답 코드
+# # 정답 코드
 
-# n을 입력받습니다.
-n = int(input())
-num = 1
+# # n을 입력받습니다.
+# n = int(input())
+# num = 1
 
-# 2차원 배열을 구현합니다.
-arr = [
-    [0 for _ in range(n)]
-    for _ in range(n)
-]
+# # 2차원 배열을 구현합니다.
+# arr = [
+#     [0 for _ in range(n)]
+#     for _ in range(n)
+# ]
 	
-# 배열의 숫자를 채웁니다.
-for i in range(n):
-	for j in range(n):
-		arr[j][i] = num
-		num += 1
+# # 배열의 숫자를 채웁니다.
+# for i in range(n):
+# 	for j in range(n):
+# 		arr[j][i] = num
+# 		num += 1
 		
-# 채워진 배열을 출력합니다.
-for row in arr:
-	for elem in row:
-		print(elem, end=" ")
-	print()
+# # 채워진 배열을 출력합니다.
+# for row in arr:
+# 	for elem in row:
+# 		print(elem, end=" ")
+# 	print()
