33718b3eb29976f943d799c7771f7c0c3fb2e37f
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-20 20:44:34 +0900

    Title: 문자열의 길이 출력하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240520/문자열의 길이 출력하기/print-string's-range.py
@@ -0,0 +1,14 @@
+# # 문자열의 길이
+
+# # len() 함수: 입력으로 주어진 문자열의 길이 반환
+# given_input = input()
+# print(len(given_input))
+# # 입력 >> hello
+# # 출력 5
+
+
+# 문제
+first_string = input()
+second_string = input()
+
+print(len(first_string) + len(second_string))
