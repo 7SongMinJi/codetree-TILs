5fb50a8f21eadc621468c368be3fe0a366fd436d
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-21 15:22:11 +0900

    Title: 공백을 기준으로 출력, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240521/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,12 @@
+first_input = input()
+second_input = input()
+
+for elem in first_input:
+    if elem == " ":
+        continue
+    print(elem, end="")
+
+for elem in second_input:
+    if elem == " ":
+        continue
+    print(elem, end="")
