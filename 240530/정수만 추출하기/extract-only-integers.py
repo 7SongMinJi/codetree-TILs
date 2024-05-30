85c75ff5e409b4692a5b2c103a75789efd65291e
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-05-30 16:06:09 +0900

    Title: 정수만 추출하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240530/정수만 추출하기/extract-only-integers.py
+++ 240530/정수만 추출하기/extract-only-integers.py
@@ -1,17 +1,17 @@
-# a, b = input().split()
-# a_digit, b_digit = [], []
-# for elem in a:
-#     if elem.isdigit() == True:
-#         a_digit.append(elem)
-#     else:
-#         break
+a, b = input().split()
+a_digit, b_digit = [], []
+for elem in a:
+    if elem.isdigit() == True:
+        a_digit.append(elem)
+    else:
+        break
 
-# for elem in b:
-#     if elem.isdigit() == True:
-#         b_digit.append(elem)
-#     else:
-#         break
+for elem in b:
+    if elem.isdigit() == True:
+        b_digit.append(elem)
+    else:
+        break
 
-# print(int("".join(a_digit)) + int("".join(b_digit)))
+print(int("".join(a_digit)) + int("".join(b_digit)))
 
 # // 처음엔 계속 틀림. isdigit()을 digit()이라 씀 & string일 경우엔 append() 사용 불가능. list일 때만 가능!
