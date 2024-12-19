152baf678691f57834b377ad9dd3fad87a11a982
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2024-12-19 19:55:52 +0900

    Title: 단어 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241219/단어 출력/print-word.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello";
+
+//     return 0;
+// }
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello";
+
+    return 0;
+}
