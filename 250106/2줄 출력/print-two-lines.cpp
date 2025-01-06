655ce7de1f9c55d4812f4d21160198f7ce26eddd
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-06 17:29:26 +0900

    Title: 2줄 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/2줄 출력/print-two-lines.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     cout << "Hello World";
+//     cout << endl;
+//     cout << "C++ is Fun";
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     cout << "Hello World" << endl << "C++ is Fun";
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello World\nC++ is Fun";
+
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello" << endl << "World";
+
+    return 0;
+}
