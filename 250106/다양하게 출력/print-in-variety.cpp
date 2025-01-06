9ea9cfac94de2afb52e2bf4b8e0b59190b305e71
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-06 17:40:37 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+// 그냥 소수 출력할 땐 소수점 6자리까지만 출력된다는 거 잊지 말기
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year" << endl << 365 << "\nCircumference rate\n" << "3.1415926535";
+
+    return 0;
+}
