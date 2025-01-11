43e0baec72344778b643715bdbabe55287ec4106
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-11 12:31:00 +0900

    Title: 정수 선언하고 뺄셈, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250111/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,51 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a;
+//     a = 5;
+//     cout << a;
+// }
+
+// # include <iostream>
+// using namespace std;
+
+// int main() {
+//     string a = "toy";
+//     double b = 3.5346;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a = 5;
+//     int b = 9;
+//     int c = a + b;
+//     cout << "c = " << c;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a = 5, b = 9;
+//     int c = a + b;
+//     cout << "c = " << c;
+
+//     return 0;
+// }
+
+// <문제: 정수 선언하고 뺼셈>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 97, b = 13;
+    int c = a - b;
+    cout << a << " - " << b << " = " << c;
+
+    return 0;
+}
