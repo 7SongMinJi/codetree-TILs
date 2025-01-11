06e986be6e0ab76efa5e0502dc387720e7c24730
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-11 12:39:39 +0900

    Title: 세 정수형 변수 선언, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250111/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     int a = 5;
+//     cout << "A is " << a << endl;
+
+//     string b = "apple";
+//     cout << "B is " << b << endl;
+
+//     cout << "A is "<< a << " and B is " << b;
+
+//     return 0;
+// }
+
+// <문제: 세 정수형 변수 선언>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 7, b = 23, c = 30;
+
+    cout << a << " + " << b << " = " << c;
+
+    return 0;
+}
