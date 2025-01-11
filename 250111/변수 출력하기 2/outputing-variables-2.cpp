8a85fddac69796d5000504c6ca06ea6e484bce5e
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-11 12:43:09 +0900

    Title: 변수 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250111/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,14 @@
+// <문제: 변수 출력하기 2>
+// 느낌표는 특수 문자가 아님! 그냥 ! 이렇게 쓰면 됨
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 3;
+    char b = 'C';
+
+    cout << b << "!.....!" << a;
+
+    return 0;
+}
