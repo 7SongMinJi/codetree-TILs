92d63c4256ed71c80b80e800fda2465105b724cf
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-12 13:23:46 +0900

    Title: 변수 값 교체하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250112/변수 값 교체하기/replacing-variable-values.cpp
@@ -0,0 +1,26 @@
+// // <변수 값 변경>
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int a = 5;
+//     cout << "A is " << a << endl;
+
+//     a = 3;
+//     cout << "A is " << a;
+
+//     return 0;
+// }
+
+// <문제: 변수 값 교체하기>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    a = 6;
+    cout << a;
+}
