e2786c24cbb6cd5aac9997cf45035448aa7ed5d9
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-13 21:37:59 +0900

    Title: 입력받은 값과 합 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250113/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,13 @@
+// <문제: 입력받은 값과 합 출력>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a << " " << b << " " << a + b;
+    return 0;
+}
