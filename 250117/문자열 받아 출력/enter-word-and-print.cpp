158e2fa6ed6697fd00af0fc02be5b5b8502c162d
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-17 18:55:05 +0900

    Title: 문자열 받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,33 @@
+// // <문자열 입력>
+
+// // c++에서 문자열 1개 입력은 string type을 이용해 받을 수 있음
+// // string 배열을 이용하기 위해서는 string 헤더를 포함시켜주어야 함
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+
+//     string a;
+//     cin >> a;
+//     cout << a;
+
+//     return 0;
+// }
+
+// <문제: 문자열 받아 출력>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string s;
+
+    cin >> s;
+    cout << s;
+    
+    return 0;
+}
