b2bbe0f6aaa019eb743af86a7246aae69cb76958
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-17 19:44:45 +0900

    Title: 두수의 곱과 몫, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,15 @@
+// <문제: 두 수의 곱과 몫>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b;
+
+    return 0;
+}
