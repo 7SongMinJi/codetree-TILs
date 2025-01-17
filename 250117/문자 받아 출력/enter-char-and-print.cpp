bf5aa01c466ad4598ea89386a4658e4f0da129e7
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-17 18:50:56 +0900

    Title: 문자 받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/문자 받아 출력/enter-char-and-print.cpp
@@ -0,0 +1,30 @@
+// // <문자 입력>
+
+// // C++에서 문자 1개 입력은 char 변수를 이용하여 받을 수 있음
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+    
+//     char a;
+//     cin >> a;
+//     cout << a;
+
+//     return 0;
+// }
+
+
+// <문제: 문자 받아 출력>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char a;
+    cin >> a;
+    cout << a;
+
+    return 0;
+}
