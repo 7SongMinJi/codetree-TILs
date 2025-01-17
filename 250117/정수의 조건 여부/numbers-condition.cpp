dd4316853b29aa85462cf0674e7a6e2e40e235bd
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-17 21:12:05 +0900

    Title: 정수의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,18 @@
+// <문제: 정수의 조건 여부>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a >= 113) {
+        cout << "1";
+    }
+    else {
+        cout << "0";
+    }
+    
+    return 0;
+}
