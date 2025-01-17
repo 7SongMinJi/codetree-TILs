e603ef10e559a9d2a52b9ce28fbfba50c19cbc7d
7SongMinJi <129584746+7SongMinJi@users.noreply.github.com>
2025-01-17 19:21:10 +0900

    Title: 입력받아 출력 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,15 @@
+// <문제: 입력받아 출력 2>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    cout << a << b;
+    return 0;
+}
